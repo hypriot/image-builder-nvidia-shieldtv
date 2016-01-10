d59311461f0039575411ab26b8db6d0584a7a3e7
Dieter Reuter <dieter.reuter@me.com>
2016-01-10 15:06:41 +0100

    install package parted with chroot

--- build.sh
+++ build.sh
@@ -8,27 +8,73 @@ fi
 
 ### setting up some important variables to control the build process
 
+# device specific settings
+IMAGE_NAME="sd-card-nvidia-shieldtv.img"
+IMAGE_ROOTFS_PATH="/image-rootfs.tar.gz"
+QEMU_ARCH="aarch64"
+
 # where to store our created sd-image file
-BUILD_RESULT="/workspace"
+BUILD_RESULT_PATH="/workspace"
+BUILD_PATH="/build"
 
 # where to store our base file system
 ROOTFS_TAR="rootfs-arm64.tar.gz"
-ROOTFS_TAR_PATH="${BUILD_RESULT}/${ROOTFS_TAR}"
+ROOTFS_TAR_PATH="${BUILD_RESULT_PATH}/${ROOTFS_TAR}"
 
 # what kernel to use
 KERNEL_DATETIME=${KERNEL_DATETIME:="20151103-193133"}
 KERNEL_VERSION=${KERNEL_VERSION:="4.1.12"}
 
-# building the name of our sd-card image file
-IMAGE_NAME="sd-card-nvidia-shieldtv.img"
-
 # size of root and boot partion
 ROOT_PARTITION_SIZE="800M"
+
+# create build directory for assembling our image filesystem
+rm -rf "${BUILD_PATH}"
+mkdir -p "${BUILD_PATH}"
 
 # download our base root file system
 if [ ! -f "${ROOTFS_TAR_PATH}" ]; then
   wget -q -O ${ROOTFS_TAR_PATH} https://github.com/hypriot/os-rootfs/releases/download/v0.4/${ROOTFS_TAR}
 fi
+
+# extract root file system
+tar -xzf "${ROOTFS_TAR_PATH}" -C "${BUILD_PATH}"
+
+# register qemu-arm with binfmt
+update-binfmts --enable qemu-"${QEMU_ARCH}"
+
+# set up mount points for pseudo filesystems
+mkdir -p "${BUILD_PATH}/{proc,sys,dev/pts}"
+
+mount -o bind /dev "${BUILD_PATH}/dev"
+mount -o bind /dev/pts "${BUILD_PATH}/dev/pts"
+mount -t proc none "${BUILD_PATH}/proc"
+mount -t sysfs none "${BUILD_PATH}/sys"
+
+# make our build directory the current root
+# and install additional packages
+chroot ${BUILD_PATH} /bin/bash <<"EOF"
+# set up /etc/resolv.conf
+export DEST=$(readlink -m /etc/resolv.conf)
+mkdir -p $(dirname $DEST)
+echo "nameserver 8.8.8.8" > $DEST
+
+# install parted (for online disk resizing)
+apt-get update
+apt-get install -y parted
+
+# set device label
+echo 'HYPRIOT_DEVICE="NVIDIA ShieldTV"' >> /etc/os-release
+exit
+EOF
+
+umount -l "${BUILD_PATH}/dev/pts" || true
+umount -l "${BUILD_PATH}/dev" || true
+umount -l "${BUILD_PATH}/proc" || true
+umount -l "${BUILD_PATH}/sys" || true
+
+# package image rootfs
+tar -czf "${IMAGE_ROOTFS_PATH}" -C "${BUILD_PATH}" .
 
 # create the image and add a single ext4 filesystem
 # --- important settings for NVIDIA ShieldTV SD card
@@ -49,17 +95,17 @@ mkfs ext4 /dev/sda1
 
 # import base rootfs
 mount /dev/sda1 /
-tar-in ${ROOTFS_TAR_PATH} / compress:gzip
+tar-in "${IMAGE_ROOTFS_PATH}" / compress:gzip
 EOF
 
 # log image partioning
 fdisk -l /${IMAGE_NAME}
 
 # test sd-image that we have built
-rspec --format documentation --color /${BUILD_RESULT}/test
+rspec --format documentation --color ${BUILD_RESULT_PATH}/test
 
 # ensure that the travis-ci user can access the sd-card image file
 umask 0000
 
 # compress image
-pigz --zip -c "${IMAGE_NAME}" > "${BUILD_RESULT}/${IMAGE_NAME}.zip"
+pigz --zip -c "${IMAGE_NAME}" > "${BUILD_RESULT_PATH}/${IMAGE_NAME}.zip"
