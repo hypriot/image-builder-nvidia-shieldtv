327b9c0ca839178462c580beb5e050aea4cf26c6
Dieter Reuter <dieter.reuter@me.com>
2016-01-11 20:04:11 +0100

    add hypriot customizing

--- devicefiles/chroot-script.sh
+++ devicefiles/chroot-script.sh
@@ -3,15 +3,38 @@ set -x
 
 # device specific settings
 HYPRIOT_DEVICE="NVIDIA ShieldTV"
+HYPRIOT_GROUPNAME="docker"
+HYPRIOT_USERNAME="pirate"
+HYPRIOT_PASSWORD="hypriot"
 
 # set up /etc/resolv.conf
 DEST=$(readlink -m /etc/resolv.conf)
-mkdir -p "$(dirname "${DEST}")"
-echo "nameserver 8.8.8.8" > "${DEST}"
+mkdir -p $(dirname $DEST)
+echo "nameserver 8.8.8.8" > $DEST
 
-# install parted (for online disk resizing)
 apt-get update
+
+#FIXME: has to be moved to hypriot/os-rootfs
+# install parted (for online disk resizing)
 apt-get install -y parted
 
+#FIXME: has to be moved to hypriot/os-rootfs
+# install sudo (for our default user)
+apt-get install -y sudo
+
+# install Hypriot group and user
+addgroup --system --quiet $HYPRIOT_GROUPNAME
+useradd -m pirate --group $HYPRIOT_GROUPNAME --shell /bin/bash
+echo "$HYPRIOT_USERNAME:$HYPRIOT_PASSWORD" | /usr/sbin/chpasswd
+# add user to sudoers group
+echo "$HYPRIOT_USERNAME ALL=NOPASSWD: ALL" > /etc/sudoers.d/user-$HYPRIOT_USERNAME
+chmod 0440 /etc/sudoers.d/user-$HYPRIOT_USERNAME
+
+#FIXME: has to be removed in hypriot/os-rootfs
+# disable SSH root login
+sed -i 's|PermitRootLogin yes|PermitRootLogin without-password|g' /etc/ssh/sshd_config
+# remove/disable root password
+passwd -d root
+
 # set device label
-echo "HYPRIOT_DEVICE=\"${HYPRIOT_DEVICE}\"" >> /etc/os-release
+echo "HYPRIOT_DEVICE=\"$HYPRIOT_DEVICE\"" >> /etc/os-release
