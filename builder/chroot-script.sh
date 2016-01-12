2a44a1726bade443fc3eb744666dcc269c3c4b61
Dieter Reuter <dieter.reuter@me.com>
2016-01-12 20:32:56 +0100

    rootfs v0.5, apt-get upgrade already done

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -13,10 +13,6 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get update
 
 #FIXME: has to be moved to hypriot/os-rootfs
-# upgrade to latest Debian package versions
-apt-get upgrade -y
-
-#FIXME: has to be moved to hypriot/os-rootfs
 # install parted (for online disk resizing)
 apt-get install -y parted
 
